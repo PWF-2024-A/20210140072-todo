b9517da830012391ca13ac693a2d57538a48d92d
Adinda Nurhayati <adindanurhayati30@gmail.com>
2024-03-17 21:38:35 +0700

    env, mvc, route, relationship

---
+++ resources/views/todo/edit.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+
+    <x-slot name="header">
+
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+
+            {{ _('Todo') }}
+
+        </h2>
+
+    </x-slot>
+
+    <div class="py-12">
+
+        <div class="mx-auto max-w-7x1 sm:px-6 1g:px-8">
+
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-1g">
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    {{ _('Index Todo Page') }}
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</x-app-layout>
