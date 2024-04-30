abbab2dd2f58f335932601676b0ca14a1f9f5b82
Adinda Nurhayati <adindanurhayati30@gmail.com>
2024-04-30 16:37:26 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,12 @@ Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.de
 Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
 Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::middleware('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+});
 Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
 Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
 Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
